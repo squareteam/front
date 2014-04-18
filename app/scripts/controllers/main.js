f6b62942afdd1328f401973dc303181cd713bfd4
Charly POLY <cpoly55@gmail.com>
2014-04-18 18:48:37 +0200

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('squareteamApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
