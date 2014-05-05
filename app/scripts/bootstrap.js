d501aadb44079995f4d924d42b1929e2577df193
Charly POLY <cpoly55@gmail.com>
2014-05-05 19:35:19 +0200

    fix(bootstrap): fix route restriction rules

--- app/scripts/bootstrap.js
+++ app/scripts/bootstrap.js
@@ -7,10 +7,19 @@ angular.module('squareteam.app').run(function($rootScope, $location, ApiSession)
     }
   });
 
+  // TODO : find a way to prevent routing before Session restore
+
   $rootScope.$on('$routeChangeStart', function(evt, next) {
+    // If no rules, route is protected by default
+
     if (((typeof next.$$route === 'undefined' || typeof next.$$route.anonymous === 'undefined') && ApiSession.isAnonymous()) ||
-          next.$$route.anonymous !== ApiSession.isAnonymous()) {
+          typeof next.$$route !== 'undefined' && !next.$$route.anonymous && ApiSession.isAnonymous()) {
       $location.path('/login');
+    // If authenticated can't access, redirect to /home
+    } else if (typeof next.$$route !== 'undefined' && next.$$route.anonymous && !ApiSession.isAnonymous()) {
+
+      $location.path('/home');
+
     }
   });
 });
