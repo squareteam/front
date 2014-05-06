3580645a3a6c2591dfca9984d6b0c2519e9f21e5
Charly POLY <cpoly55@gmail.com>
2014-05-06 23:03:39 +0200

    feat(routing): better solution for handling access rules

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -17,36 +17,51 @@ angular
     'ngSanitize',
     'ui.router'
   ])
-  .config(function ($stateProvider/*, $urlRouterProvider*/) {
+  .config(function ($stateProvider, $urlRouterProvider) {
+
+    var sessionCheck = ['ApiSession', '$q', '$location', function(ApiSession, $q, $location) {
+      var defer = $q.defer();
+
+      function isUserAuthenticated() {
+        if (ApiSession.isAuthenticated()) {
+          defer.resolve();
+        } else {
+          defer.reject();
+          $location.path('/login');
+        }
+      }
+
+      if (ApiSession.$pristine) {
+        ApiSession.restore().then(function() {
+          isUserAuthenticated();
+        }.bind(this));
+      } else {
+        isUserAuthenticated();
+      }
+
+      return $q.promise;
+    }];
+
     $stateProvider
       .state('login', {
         url : '/login',
         templateUrl: 'views/login.html',
-        controller: 'LoginCtrl',
-        data : {
-          acl : {
-            'public' : true
-          }
-        }
+        controller: 'LoginCtrl'
       })
       .state('register', {
         url : '/register',
-        templateUrl: 'views/register.html',
-        data : {
-          acl : {
-            'public' : true
-          }
-        }
+        templateUrl: 'views/register.html'
       })
       .state('home', {
         url : '/home',
         templateUrl: 'views/home.html',
-        controller: 'HomeCtrl'
+        controller: 'HomeCtrl',
+        resolve : {
+          access : sessionCheck
+        }
       });
 
-    // $urlRouterProvider
-    //   .otherwise({
-    //     redirectTo: '/home'
-    //   });
+    $urlRouterProvider
+      .otherwise('/home');
 
   });
--- app/scripts/bootstrap.js
+++ app/scripts/bootstrap.js
@@ -1,31 +1,11 @@
 'use strict';
 
-angular.module('squareteam.app').run(function($rootScope, $state, $urlRouter, ApiSession) {
-  
-  var preventRoutingBeforeSessionLoaded = $rootScope.$on('$stateChangeStart', function(evt) { evt.preventDefault(); });
-
-  function setupRoutingACL () {
-    $rootScope.$on('$stateChangeStart', function(evt, next) {
-      if ( (!next.data || !next.data.acl) && !ApiSession.isAuthenticated() ) {
-        console.log('redirect to login (default)');
-        evt.preventDefault();
-        $state.go('login');
-      } else if (next.data && next.data.acl['public'] === false  && !ApiSession.isAuthenticated() ) {
-        console.log('redirect to login (public set to false)');
-        evt.preventDefault();
-        $state.go('login');
-      }
-
-    });
-  }
+angular.module('squareteam.app').run(function(ApiSession) {
 
   ApiSession.restore().then(function(errorIfAny) {
     if (errorIfAny && errorIfAny === 'auth.invalid') {
       // redirect to login page with flash message
     }
-    preventRoutingBeforeSessionLoaded();
-    setupRoutingACL();
-    $urlRouter.sync();
   }.bind(this));
 
 });
