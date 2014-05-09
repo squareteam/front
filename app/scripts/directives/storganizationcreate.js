02c1aaf44ec343ae2f1e542661320c402cc8a2a9
Charly POLY <cpoly55@gmail.com>
2014-05-09 10:24:25 +0200

    feat(stOrganizationCreate): working form

--- app/scripts/directives/storganizationcreate.js
+++ app/scripts/directives/storganizationcreate.js
@@ -6,19 +6,46 @@ angular.module('squareteam.app')
       scope: {
         forUser : '@',
         membersChooser : '@',
-        redirectTo : '@'
+        redirectPath : '@'
       },
       templateUrl: 'scripts/directives/templates/storganizationcreate.html',
       restrict: 'E',
       replace: true,
-      controller: function($scope, $element, $attrs, $location, UserRessource) {
+      controller: function($scope, $element, $attrs, $location, $http, OrganizationRessource, ApiErrors) {
+
+        function onError (response) {
+          if (response.error instanceof ApiErrors.Api) {
+            angular.forEach(response.error.getErrors(), function(errorText) {
+              if (errorText === 'Name has already been taken') {
+                $scope.createOrganizationForm.name.$setValidity('unique', false);
+              }
+            });
+          } else {
+            $scope.serverBusy = true;
+          }
+        }
+
         $scope.create = function() {
+          $scope.serverBusy = false;
+          $scope.createOrganizationForm.name.$setValidity('unique', true);
 
-          UserRessource.organizations.save({}, $scope.organization, function() {
-            $location.path($scope.redirectTo);
-          }, function() {
+          var organization = OrganizationRessource.save({}, $scope.organization, function() {
+            function redirectOnFinish () {
+              $location.path($scope.redirectPath || '/home');
+            }
 
-          });
+            if ($scope.forUser) {
+              $http.post('apis://members', {
+                'organization_id' : organization.id,
+                'user_id' : $scope.forUser,
+                'admin'   : 1
+              }).then(function() {
+                redirectOnFinish();
+              }, onError);
+            } else {
+              redirectOnFinish();
+            }
+          },onError);
         };
       }
     };
