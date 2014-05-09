9c42471928094a965c0ea234b00c0840c4f5ab94
Charly POLY <cpoly55@gmail.com>
2014-05-09 17:30:52 +0200

    refactor(HomeCtrl): move logic into ApplicationCtrl

--- app/scripts/controllers/application.js
+++ app/scripts/controllers/application.js
@@ -1,7 +1,15 @@
 'use strict';
 
 angular.module('squareteam.app')
-  .controller('ApplicationCtrl', function ($scope, ApiSession, Currentuser) {
+  .controller('ApplicationCtrl', function ($rootScope, $scope, $state, ApiSession, Currentuser) {
     $scope.session      = ApiSession;
-    $scope.currentUser = Currentuser;
+    $scope.currentUser  = Currentuser;
+
+    $scope.logout = function() {
+      ApiSession.logout().then(function() {
+        $state.go('login');
+      }.bind(this), function() {
+        $state.go('app.home');
+      }.bind(this));
+    };
   });
--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -1,17 +1,6 @@
 'use strict';
 
 angular.module('squareteam.app')
-  .controller('HomeCtrl', function ($scope, $location, Currentuser, ApiSession) {
-
-
-    $scope.organizations = $scope.currentUser.getUser().organizations;
-
-
-    $scope.logout = function() {
-      ApiSession.logout().then(function() {
-        $location.path('/login');
-      }.bind(this), function() {
-        $location.path('/home'); // FIXME ?
-      }.bind(this));
-    };
-  });
+  .controller('HomeCtrl', function (/*$scope*/) {
+    
+});
