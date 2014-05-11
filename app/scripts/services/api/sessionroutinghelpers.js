9de8245aadfe5575e5290f396a18a7c1895c5eff
Charly POLY <cpoly55@gmail.com>
2014-05-11 17:30:48 +0200

    fix(ApiSessionRoutingHelpers): use $location instead of $state for redirection to login

--- app/scripts/services/api/sessionroutinghelpers.js
+++ app/scripts/services/api/sessionroutinghelpers.js
@@ -3,57 +3,58 @@
 angular.module('squareteam.api')
   .provider('ApiSessionRoutingHelpers', function Apisessionroutinghelpers() {
 
-    this.checkAuthenticated =  ['Currentuser', 'ApiSession', 'UserRessource', '$q', '$state', '$rootScope',
-      function(Currentuser, ApiSession, UserRessource, $q, $state, $rootScope) {
-      var deferred = $q.defer();
-      
-      console.log('ApiSessionRoutingHelpers.checkAuthenticated');
+    this.checkAuthenticated =  ['Currentuser', 'ApiSession', 'UserRessource', '$q', '$state', '$location',
+      function(Currentuser, ApiSession, UserRessource, $q, $state, $location) {
 
-      function configureUser (userId) {
-        var organizations = UserRessource.organizations.query({
-          userId : userId
-        },
-        {}, // data
-        function() {
-          if (organizations.length) {
-            Currentuser.setOrganizations(organizations);
-            deferred.resolve();
+        function configureUser (userId) {
+          var organizations = UserRessource.organizations.query({
+              userId : userId
+            },
+            {}, // data
+            function() {
+              if (organizations.length) {
+                Currentuser.setOrganizations(organizations);
+                deferred.resolve();
+              } else {
+                deferred.reject();
+                console.log('redirect to organization creation');
+                $state.go('register_organization');
+              }
+            }
+          );
+        }
+
+        function isUserAuthenticated() {
+          console.log('isUserAuthenticated?', ApiSession.isAuthenticated());
+          if (ApiSession.isAuthenticated()) {
+            if (!!Currentuser.getUser().organizations && Currentuser.getUser().organizations.length) {
+              deferred.resolve();
+            } else {
+              configureUser(Currentuser.getUser().id);
+            }
           } else {
             deferred.reject();
-            console.log('redirect to organization creation');
-            $rootScope.$eval(function() {
-              $state.go('register_organization');
-            });
+            console.log('redirect to login');
+            // $state.go('login');
+            $location.path('/login');
           }
-        });
-      }
-
-      function isUserAuthenticated() {
-        console.log('isUserAuthenticated?', ApiSession.isAuthenticated());
-        if (ApiSession.isAuthenticated()) {
-          if (!!Currentuser.getUser().organizations && Currentuser.getUser().organizations.length) {
-            deferred.resolve();
-          } else {
-            configureUser(Currentuser.getUser().id);
-          }
-        } else {
-          deferred.reject();
-          $state.go('login');
         }
-      }
 
-      console.log('ApiSession.$pristine?', ApiSession.$pristine);
+        var deferred = $q.defer();
 
-      if (ApiSession.$pristine) {
-        ApiSession.restore().then(function() {
+        console.log('ApiSession.$pristine?', ApiSession.$pristine);
+
+        if (ApiSession.$pristine) {
+          ApiSession.restore().then(function() {
+            isUserAuthenticated();
+          }.bind(this));
+        } else {
           isUserAuthenticated();
-        }.bind(this));
-      } else {
-        isUserAuthenticated();
-      }
+        }
 
-      return deferred.promise;
-    }];
+        return deferred.promise;
+      }
+    ];
 
     this.$get = function() {};
   });
