028c5ba9e51baa0152400199ff47c17c7c310f14
Charly POLY <cpoly55@gmail.com>
2014-05-12 22:38:42 +0200

    fix(app): remove autoload of session
    Closes #8

--- app/scripts/bootstrap.js
+++ app/scripts/bootstrap.js
@@ -1,11 +1,5 @@
 'use strict';
 
-angular.module('squareteam.app').run(function(ApiSession) {
+// angular.module('squareteam.app').run(function(ApiSession) {
 
-  ApiSession.restore().then(function(errorIfAny) {
-    if (errorIfAny && errorIfAny === 'auth.invalid') {
-      // redirect to login page with flash message
-    }
-  }.bind(this));
-
-});
+// });
