6380256a2d2a0869387b3d057434ca41cb8413c5
Charly POLY <cpoly55@gmail.com>
2014-05-13 22:28:18 +0200

    Release v0.0.11

--- app/scripts/initializers/modules.js
+++ app/scripts/initializers/modules.js
@@ -18,5 +18,5 @@ angular
 
 // DO NOT EDIT LINE BELOW
 //  open README.md for more explaination
-var version = '0.0.7';
+var version = '0.0.11';
 angular.module('squareteam.app').value('VERSION', version);
