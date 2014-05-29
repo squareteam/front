927e0679e01047812ec738ca07033c11f6344317
Charly POLY <cpoly55@gmail.com>
2014-05-30 00:51:08 +0200

    feat(app): design refactoring
    - design POC for timeline
    - dropdown menu on current user avatar
    - manage organizations pages

---
+++ app/scripts/directives/stcurrentuserblock.js
@@ -0,0 +1,32 @@
+'use strict';
+
+angular.module('squareteam.app')
+  .directive('stCurrentUserBlock', function () {
+    return {
+      restrict: 'E',
+      transclude : true,
+      templateUrl : 'scripts/directives/templates/stcurrentuserblock.html',
+      link: function postLink(scope, element) {
+        var userAvatar  = element.find('.user-avatar'),
+            dropdown    = element.find('.dropdown-menu');
+
+        dropdown.css('marginTop', '40px');
+
+        userAvatar.on('mouseover', function() {
+          dropdown.show();
+        });
+
+        dropdown.on('mouseleave', function() {
+          dropdown.hide();
+        });
+
+        dropdown.on('click', function() {
+          dropdown.hide();
+        });
+
+        scope.$watch('currentSession.isAuthenticated()', function(authenticated) {
+          element[authenticated ? 'show' : 'hide']();
+        });
+      }
+    };
+  });
