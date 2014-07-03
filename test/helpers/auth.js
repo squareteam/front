20e92c250ead90911305f419ac1782cac8774a00
Charly POLY <cpoly55@gmail.com>
2014-07-03 20:23:19 +0200

    test(helpers): add auth helpers for testing

---
+++ test/helpers/auth.js
@@ -0,0 +1,39 @@
+/* global CryptoJS */
+
+'use strict';
+
+/**
+ * Provide a mock but valid auth for testing
+ * components that require authentification
+ *
+ * @method provideAuth
+ * @param  {angular.injector} $injector
+ * @return {Function}
+ */
+function provideAuth ($injector) {
+  return function(login, token) {
+    var _login     = login  || 'charly.poly@live.fr',
+        _token     = token  || 'a99246bedaa6cadacaa902e190f32ec689a80a724aa4a1c198617e52460f74d1',
+        ApiAuth   = $injector.get('ApiAuth');
+
+    $injector.get('CurrentSession').$$user = { id : 1, name : 'test-auth', email : _login};
+    $injector.get('CurrentSession').$$auth = new ApiAuth(_login, CryptoJS.enc.Hex.parse(_token));
+  };
+}
+
+/**
+ * Provide a invalid auth for testing
+ * components that require authentification
+ *
+ * @method provideAnonymousAuth
+ * @param  {angular.injector} $injector
+ * @return {Function}
+ */
+function provideAnonymousAuth ($injector) {
+  return function() {
+    var ApiAuth   = $injector.get('ApiAuth');
+
+    $injector.get('CurrentSession').$$user = null;
+    $injector.get('CurrentSession').$$auth = new ApiAuth();
+  };
+}
