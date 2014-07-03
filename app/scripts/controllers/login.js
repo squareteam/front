b4274fae77a49e68a86d0e713f2cee41ae1a75a0
Charly POLY <cpoly55@gmail.com>
2014-07-03 21:40:15 +0200

    test(LoginCtrl): testing and improvements

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -1,19 +1,18 @@
 'use strict';
 
-// When the application is loaded to /login?email=:email
-// The LoginCtrl will try to find a st.oauth cookies containing
+// When the application is loaded to /login?email=:email&provider=:provider
+// The LoginCtrl will try to find a "st.oauth" cookies containing
 // a temporary login token to be used as password
 
 angular.module('squareteam.app')
 .controller('LoginCtrl', function ($scope, $cookies, $location, $state, ApiSession, appConfig) {
-  var login = $location.search() && $location.search().email;
+  var login     = $location.search() && $location.search().email,
+      provider  = $location.search() && $location.search().provider;
+
   $scope.login = login || '';
   $scope.oauthAuthenticating = false;
 
-  if ($cookies[appConfig.api.oauth.cookieNS]) {
-    // if (!login) {
-    //   // ask email to continue
-    // }
+  if ($cookies[appConfig.api.oauth.cookieNS] && login && provider && appConfig.api.oauth[provider]) {
     $scope.oauthAuthenticating = true;
     ApiSession.login(login, $cookies[appConfig.api.oauth.cookieNS]).then(function() {
       delete $cookies[appConfig.api.oauth.cookieNS];
