38c41945801937568072fdb9a20f5f46d25e32bc
Charly POLY <cpoly55@gmail.com>
2014-07-15 22:55:35 +0200

    feat(ApiCache): custom $http cache strategy

---
+++ test/spec/services/api/cache.js
@@ -0,0 +1,79 @@
+'use strict';
+
+describe('Service: ApiCache', function () {
+
+  // load the service's module
+  beforeEach(module('squareteam.app'));
+
+  // instantiate service
+  var ApiCache, appConfig;
+
+  beforeEach(inject(function ($injector) {
+    ApiCache = $injector.get('ApiCache');
+    appConfig = $injector.get('appConfig');
+  }));
+
+  function url (path) {
+    return appConfig.api.url + path;
+  }
+
+  it('should store request as given', function() {
+    ApiCache.put(url('project/1'), { id : 1 , name : 'test'});
+
+    expect(ApiCache.get(url('project/1'))).toEqual({ id : 1 , name : 'test'});
+  });
+
+  it('should store request as given and split if collection', function() {
+    ApiCache.put(url('projects'), [
+      { id : 1 , name : 'test'},
+      { id : 2 , name : 'tester'},
+      { id : 3 , name : 'testeeeer'}
+    ]);
+
+    expect(ApiCache.get(url('projects'))).toEqual([
+      { id : 1 , name : 'test'},
+      { id : 2 , name : 'tester'},
+      { id : 3 , name : 'testeeeer'}
+    ]);
+
+    expect(ApiCache.get(url('project/1'))).toEqual({ id : 1 , name : 'test'});
+    expect(ApiCache.get(url('project/2'))).toEqual({ id : 2 , name : 'tester'});
+    expect(ApiCache.get(url('project/3'))).toEqual({ id : 3 , name : 'testeeeer'});
+  });
+
+  describe('removing data from cache', function() {
+
+    it('should remove request data', function() {
+      ApiCache.put(url('project/1'), { id : 1 , name : 'test'});
+
+      ApiCache.remove(url('project/1'));
+
+      expect(ApiCache.get(url('project/1'))).toBeUndefined();
+    });
+
+    it('should remove request associated data', function() {
+
+      ApiCache.put(url('projects'), [
+        { id : 1 , name : 'test'},
+        { id : 2 , name : 'tester'},
+        { id : 3 , name : 'testeeeer'}
+      ]);
+
+      ApiCache.remove(url('projects'));
+
+      expect(ApiCache.get(url('projects'))).toBeUndefined();
+
+      expect(ApiCache.get(url('project/1'))).toBeUndefined();
+      expect(ApiCache.get(url('project/2'))).toBeUndefined();
+      expect(ApiCache.get(url('project/3'))).toBeUndefined();
+
+    });
+
+  });
+
+
+  afterEach(function() {
+    ApiCache.removeAll();
+  });
+
+});
