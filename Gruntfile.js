30924ac8a6b32cc0c297b008304967548a59a193
Charly POLY <cpoly55@gmail.com>
2014-08-20 19:01:04 +0200

    chore(Gruntfile): remove css related tasks for testing
    Closes #48

--- Gruntfile.js
+++ Gruntfile.js
@@ -442,8 +442,8 @@ module.exports = function (grunt) {
 
   grunt.registerTask('test', [
     'clean:server',
-    'concurrent:test',
-    'autoprefixer',
+    // 'concurrent:test',
+    // 'autoprefixer',
     'connect:test',
     'karma'
   ]);
