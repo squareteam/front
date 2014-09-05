92cf2f0bb3dfc907a86c88b4e85bae4dc549e7fe
Charly POLY <cpoly55@gmail.com>
2014-09-05 10:22:39 +0200

    test(Organizations): full coverage

---
+++ test/spec/controllers/organizations.js
@@ -0,0 +1,51 @@
+/* global apiURL, provideAuth */
+
+'use strict';
+
+describe('Controller: Organizations', function () {
+
+  beforeEach(module('squareteam.app'));
+
+  var Organizations, ApiSession, url,
+      $state, $q, $rootScope, $httpBackend,
+      scope;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function ($controller, $injector) {
+
+    $state        =  $injector.get('$state');
+    $rootScope    =  $injector.get('$rootScope');
+    $q            =  $injector.get('$q');
+    $httpBackend  =  $injector.get('$httpBackend');
+
+    ApiSession  =  $injector.get('ApiSession');
+
+    url = apiURL($injector);
+
+    provideAuth($injector)();
+
+    $httpBackend.expectGET( url('users/1/organizations') ).respond(200, '{"data":[{"id":1,"name":"FMB","description":"test test"}]}');
+
+    scope = $rootScope.$new();
+    Organizations = $controller('Organizations', {
+      $scope              : scope,
+      currentOrganization : {
+        id : 1
+      }
+    });
+
+    scope.$digest();
+
+    $httpBackend.flush();
+
+  }));
+
+  it('should expose user organizations', function() {
+
+    scope.$digest();
+
+    expect(scope.organizations.length).toEqual(1);
+
+  });
+
+});
