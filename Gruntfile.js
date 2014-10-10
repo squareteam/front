db93613ca69a1e69adc510a790261e0075993414
Charly POLY <cpoly55@gmail.com>
2014-10-10 11:01:47 +0200

    chore(build): drop imagemin usage for now

--- Gruntfile.js
+++ Gruntfile.js
@@ -285,16 +285,16 @@ module.exports = function (grunt) {
       }
     },
 
-    imagemin: {
-      dist: {
-        files: [{
-          expand: true,
-          cwd: '<%= yeoman.app %>/images',
-          src: '{,*/}*.{png,jpg,jpeg,gif}',
-          dest: '<%= yeoman.dist %>/images'
-        }]
-      }
-    },
+    // imagemin: {
+    //   dist: {
+    //     files: [{
+    //       expand: true,
+    //       cwd: '<%= yeoman.app %>/images',
+    //       src: '{,*/}*.{png,jpg,jpeg,gif}',
+    //       dest: '<%= yeoman.dist %>/images'
+    //     }]
+    //   }
+    // },
 
     svgmin: {
       dist: {
@@ -381,7 +381,7 @@ module.exports = function (grunt) {
       ],
       dist: [
         'sass',
-        'imagemin',
+        // 'imagemin',
         'svgmin'
       ]
     },
