2ea94d3257b451c7a01f8d0287c2a8ab21bddae5
Charly POLY <cpoly55@gmail.com>
2014-10-13 20:24:06 +0200

    chore(build): replace ngmin by ngannotate
    Closes #79

--- Gruntfile.js
+++ Gruntfile.js
@@ -324,10 +324,10 @@ module.exports = function (grunt) {
       }
     },
 
-    // ngmin tries to make the code safe for minification automatically by
+    // ngAnnotate tries to make the code safe for minification automatically by
     // using the Angular long form for dependency injection. It doesn't work on
     // things like resolve or inject so those have to be done manually.
-    ngmin: {
+    ngAnnotate: {
       dist: {
         files: [{
           expand: true,
@@ -474,7 +474,7 @@ module.exports = function (grunt) {
     'useminPrepare',
     'concurrent:dist',
     'concat',
-    'ngmin',
+    'ngAnnotate',
     'copy:dist',
     'cssmin',
     'uglify',
